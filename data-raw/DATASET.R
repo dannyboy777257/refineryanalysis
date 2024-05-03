0c7295ef9e2e812db8740a75368f0ce7bef4ae4d
dannyboy777257 <danielvovk0@gmail.com>
2024-05-02 19:52:47 -0600

    adding publishing ability

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,24 +3,24 @@
 # some errors are from users filling out forms incorrectly etc :(
 # some refinery having multiple duplicates and major issues due to OCR need to fix with more time
 masterProd <- refineryanalysis::dataCleaningProduction(
-  readRDS(file = "C:/Users/danie/Documents/Fin488/refineryFinal/Masterfiles/finalProduction.rds"))
+  readRDS(file = "./data-raw/finalProduction.rds"))
 
 masterProd[is.na(masterProd)] <- 0
 usethis::use_data(masterProd, overwrite = TRUE)
 
 # Master data for deliveries for refineries
 masterDeliveries <- refineryanalysis::dataCleaningDeliveries(
-  readRDS(file = "C:/Users/danie/Documents/Fin488/refineryFinal/Masterfiles/finalDeliveries.rds"))
+  readRDS(file = "./data-raw/finalDeliveries.rds"))
 
 usethis::use_data(masterDeliveries, overwrite = TRUE)
 
 # Master data for receipts for refineries
 masterReceipts <- refineryanalysis::dataCleaningReceipts(
-  readRDS(file = "C:/Users/danie/Documents/Fin488/refineryFinal/Masterfiles/finalReceipts.rds"))
+  readRDS(file = "./data-raw/finalReceipts.rds"))
 
 usethis::use_data(masterReceipts, overwrite = TRUE)
 
 # shape file for refineries in texas
-refShape <- refineryanalysis::shapeClean("C:/Users/danie/Documents/Fin488/refineryFinal/shapeRef/Petroleum_Refinery.shp")
+refShape <- refineryanalysis::shapeClean("./data-raw/Petroleum_Refinery.shp")
 
 usethis::use_data(refShape, overwrite = TRUE)
